fc279903d0c9c09c3b5c28b83df3c8c714b896ca
ELIAS SAARENKOSKI <eliassaarenkoski@gmail.com>
2026-08-20 15:42:34 +0300

    joo

---
+++ mod2/testi.py
@@ -0,0 +1,5 @@
+gramma = input ("kuinka monta grammaa:")
+kilo = int(gramma) // 1000
+vaingrammat = int(gramma) % 1000
+
+print(f"määrä kiloin ja grammoina: {kilo} kg {vaingrammat} g")
---
+++ mod2/tuntitehtävä3.py
@@ -0,0 +1,3 @@
+gramma = input ("kuinka monta grammaaa:")
+kilo = 1000 * (int(gramma))
+print("määrä kiloina ja grammoina") + (str(kilo) + "kg" + gramma + "g")
